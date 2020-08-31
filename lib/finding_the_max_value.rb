a33e0fa500feaac7b2edcd7e82c5d823cbe641e8
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-31 07:15:52 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
+array = [17,3,8,12,13,41,11,16,33]
+
 def find_max_value(array)
-  # Add your solution here
+  i = 0 
+  max = array[0]
+    while array[i] do
+           if array[i] > max
+           max = array[i]
+         end
+      i += 1
+    end
+ max
 end
+binding.pry
